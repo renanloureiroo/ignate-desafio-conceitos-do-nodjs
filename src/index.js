9748106c720f7171a9eb907a6f42ebabac6fe839
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-03 20:51:39 -0300

    route create user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  if (!name || !username) {
+    return response.status(400).json({
+      error: "Missing name or username"
+    })
+  }
+
+  const user = {
+    id: uuidV4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user)
+
+  return response.status('201').json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
